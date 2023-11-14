0bbaa738b827cdd235e95f3796e3467964cf6481
Henry <henry@crayond.co>
2023-11-14 14:43:15 +0530

    build isshes

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from 'vite-plugin-dts';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
-    port: 3000,
-  },
-  preview: {
-    port: 4000,
+    port: 3001,
   },
  
   build: {
     minify: true,
     lib: {
-      entry: "src/components",
+      entry: resolve(__dirname, 'src/components'),
+      // entry: "src/components",
       fileName: "index",
       formats: ["es"],
     },
   },
+  plugins: [react(),
+    dts({ include: ['src'] })
+  ],
 });
