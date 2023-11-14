5fdab02b7b9ca4dc422c5d4a5e56c9ab7bc2fa16
Henry <henry@crayond.co>
2023-11-14 13:01:49 +0530

    build isshes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig } from "vite";
-import { terser } from 'rollup-plugin-terser';
+import dts from 'vite-plugin-dts';
 
 
 // https://vitejs.dev/config/
@@ -13,41 +12,13 @@ export default defineConfig({
   preview: {
     port: 4000,
   },
-  resolve: {
-    alias: [
-      {
-        find: "@components",
-        replacement: path.join(__dirname, "./src/components"),
-      },
-
-      {
-        find: "@lib",
-        replacement: path.join(__dirname, "./src/lib"),
-      },
-      {
-        find: "@utils",
-        replacement: path.join(__dirname, "./src/utils"),
-      },
-      {
-        find: "@store",
-        replacement: path.join(__dirname, "./src/store"),
-      },
-      {
-        find: "@atoms",
-        replacement: path.join(__dirname, "./src/atoms"),
-      },
-    ],
-  },
+ 
   build: {
     minify: true,
     lib: {
       entry: "src/components",
       fileName: "index",
       formats: ["es"],
-    },
-    rollupOptions: {
-      plugins: [terser()],
-      external: ["react", "react-dom"],
     },
   },
 });
